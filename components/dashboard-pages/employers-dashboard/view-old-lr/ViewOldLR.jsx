be812bf9a071af3b4e14d7282437b707afbd889d
Mounish Soni <mounishsoni116@gmail.com>
2024-06-15 10:46:19 -0400

    added email address to lr view

--- components/dashboard-pages/employers-dashboard/view-old-lr/ViewOldLR.jsx
+++ components/dashboard-pages/employers-dashboard/view-old-lr/ViewOldLR.jsx
@@ -12,9 +12,9 @@ const ViewOldLR = ({ fetchedLRdata }) => {
                     <span><b>Comapny Name: RAFTAAR LOGISTICS </b></span><br />
                     <span><b>Head Office: </b>51 and 52 Sinde Colony, S R P Road, Navapura, Vadodara, Gujarat 390001</span> <br />
                     <span><b>Mobile No: </b>7016229891</span>
-                    {/* <span>
-                    <t></t> | <t></t><b>Email: </b>margisoni031@gmail.com
-                    </span> */}
+                    <span>
+                    <t></t> | <t></t><b>Email: </b>raftaarlogistic@gmail.com
+                    </span>
                     {/* <span>
                     <t></t> | <t></t><b>Website: </b>raftaarlogistics.com
                     </span>  */}
@@ -109,9 +109,9 @@ const ViewOldLR = ({ fetchedLRdata }) => {
                     <span><b>Comapny Name: RAFTAAR LOGISTICS </b></span><br />
                     <span><b>Head Office: </b>51 and 52 Sinde Colony, S R P Road, Navapura, Vadodara, Gujarat 390001</span> <br />
                     <span><b>Mobile No: </b>7016229891</span>
-                    {/* <span>
-                    <t></t> | <t></t><b>Email: </b>info@raftaarlogistics.com
-                    </span> */}
+                    <span>
+                    <t></t> | <t></t><b>Email: </b>raftaarlogistic@gmail.com
+                    </span>
                     {/* <span>
                     <t></t> | <t></t><b>Website: </b>raftaarlogistics.com
                     </span>  */}
@@ -206,9 +206,9 @@ const ViewOldLR = ({ fetchedLRdata }) => {
                     <span><b>Comapny Name: RAFTAAR LOGISTICS </b></span><br />
                     <span><b>Head Office: </b>51 and 52 Sinde Colony, S R P Road, Navapura, Vadodara, Gujarat 390001</span> <br />
                     <span><b>Mobile No: </b>7016229891</span>
-                    {/* <span>
-                    <t></t> | <t></t><b>Email: </b>info@raftaarlogistics.com
-                    </span> */}
+                    <span>
+                    <t></t> | <t></t><b>Email: </b>raftaarlogistic@gmail.com
+                    </span>
                     {/* <span>
                     <t></t> | <t></t><b>Website: </b>raftaarlogistics.com
                     </span>  */}
