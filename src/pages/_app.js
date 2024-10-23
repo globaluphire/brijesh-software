6a0d72c2f2d81b5845cbfae3e1ed9afd789a5643
Mounish Soni <mounishsoni116@gmail.com>
2024-10-23 18:39:48 -0400

    config supabase firebase to login page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,14 +4,33 @@ import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 import { StyleClass } from 'primereact/styleclass';
+import { getDecryptedItem } from "@/utils/encryptedStorage";
+import { setUserData } from "@/features/slice/initialStatesSlice";
+import { useEffect } from "react";
+import { Provider } from "react-redux";
+import { store } from "@/app/store";
 
 export default function App({ Component, pageProps }) {
   const primereactConfig = {
     ripple: true,
   };
+
+  useEffect(() => {
+      try {
+          const user = JSON.parse(getDecryptedItem("user"));
+          if (user.id) {
+              store.dispatch(setUserData(user));
+          }
+      } catch (e) {
+          console.warn(e);
+      }
+  }, []);
+
   return (
     <PrimeReactProvider value={primereactConfig}>
-      <Component {...pageProps} />
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
     </PrimeReactProvider>
   );
 }
